8200cfd3472831dc7a0a637dfe5aa04dea01bb86
michael <mderazon@hotmail.com>
2015-07-13 00:46:24 +0300

    a little more realistic tests
    - use “real” browser zombie.js for end to end tests
    - testing iOS is still hard
    - update dependencies

---
+++ test/lib/browser.js
@@ -0,0 +1,40 @@
+var Browser = require('zombie')
+var express = require('express')
+var deeplink = require('../..')
+
+var noop = function () {}
+
+module.exports = function (ua) {
+  var obj = {}
+  Browser.localhost('localhost', 3000)
+  var browser = new Browser()
+  browser.userAgent = ua
+  var app = express()
+  var server
+
+  obj.go = function (url, opts, callback) {
+
+    app.get('/', deeplink(opts))
+
+    server = app.listen(3000)
+
+    var loc = 0
+    browser.on('event', function (e, target) {
+      if (target.location && e.type === 'load') {
+        if (loc === 1) {
+          callback(target.location.href)
+        }
+        loc++
+      }
+
+    })
+
+    browser.visit('/?url=' + url, noop)
+  }
+
+  obj.close = function () {
+    server.close()
+  }
+
+  return obj
+}
--- test/test.js
+++ test/test.js
@@ -1,109 +1,104 @@
 /*
-  global suite: true
-  global test: true
+  global describe: true
+  global it: true
+  global beforeEach: true
+  global afterEach: true
 */
 
 var assert = require('chai').assert
 var device = require('./lib/device')
-var deeplink = require(__dirname + '/../lib/public/script.js')
+var Browser = require('./lib/browser')
 
 var url = 'app://test'
-var fallback = 'https://vandelay.com'
+var fallback = 'https://vandelay.com/'
 var android_package_name = 'ind.vandelay.art'
 var ios_store_link = 'https://itunes.apple.com/us/app/art-vandelay/id556462755?mt=8&uo=4'
 
-// mock DOM window
-function mock_window (ua) {
-  return {
-    navigator: {
-      userAgent: ua
-    },
-    location: ''
-  }
-}
+describe('android', function () {
+  var browser
 
-suite('android', function () {
-  test('android device should return intent', function (done) {
-    var window = mock_window(device.android)
-    var deep = deeplink(window)
-    deep({
-      url: url,
+  beforeEach(function () {
+    browser = Browser(device.android)
+  })
+
+  afterEach(function () {
+    browser.close()
+  })
+
+  it('should return intent on android device', function (done) {
+    browser.go(url, {
       fallback: fallback,
       android_package_name: android_package_name
-    })
-
-    assert.equal(window.location, 'intent://test#Intent;scheme=app;package=ind.vandelay.art;end;')
-    done()
-  })
-
-  test('android device with no package name defined should return fallback', function (done) {
-    var window = mock_window(device.android)
-    var deep = deeplink(window)
-    deep({
-      url: url,
-      fallback: fallback
-    })
-
-    assert.equal(window.location, fallback)
-    done()
-  })
-
-})
-
-suite('ios', function () {
-  test('ios device should return deep link', function (done) {
-    var window = mock_window(device.ios)
-    var deep = deeplink(window)
-    deep({
-      url: url,
-      fallback: fallback,
-      ios_store_link: ios_store_link
-    })
-
-    assert.equal(window.location, url)
-    done()
-  })
-
-  test('ios device with no deeplink url defined should return fallback', function (done) {
-    var window = mock_window(device.ios)
-    var deep = deeplink(window)
-    deep({
-      fallback: fallback,
-      ios_store_link: ios_store_link
-    })
-
-    assert.equal(window.location, 'https://vandelay.com')
-    done()
-  })
-
-  test('ios device with deeplink but no app installed should go to app store', function (done) {
-    var window = mock_window(device.ios)
-    var deep = deeplink(window)
-    deep({
-      url: url,
-      fallback: fallback,
-      ios_store_link: ios_store_link
-    })
-
-    setTimeout(function () {
-      assert.equal(window.location, ios_store_link)
+    }, function (res) {
+      assert.equal(res, 'intent://test#Intent;scheme=app;package=ind.vandelay.art;end;')
       done()
-    }, 30)
+    })
+  })
+
+  it('should return the fallback url when no package name defined in android', function (done) {
+    browser.go(url, {
+      fallback: fallback
+    }, function (res) {
+      assert.equal(res, fallback)
+      done()
+    })
   })
 
 })
 
-suite('others', function () {
-  test('unsupported device should go to fallback', function (done) {
-    var window = mock_window('desktop useragent')
-    var deep = deeplink(window)
-    deep({
-      url: url,
-      fallback: fallback
+describe('ios', function () {
+  var browser
+
+  beforeEach(function () {
+    browser = Browser(device.ios)
+  })
+
+  afterEach(function () {
+    browser.close()
+  })
+
+  it('should return deeplink url on ios device', function (done) {
+    browser.go(url, {
+      fallback: fallback,
+      ios_store_link: ios_store_link
+    }, function (res) {
+      assert.equal(res, url)
+      done()
     })
 
-    assert.equal(window.location, fallback)
-    done()
+  })
+
+  it('should return the fallback url when no ios store link defined in ios', function (done) {
+    browser.go(url, {
+      fallback: fallback
+    }, function (res) {
+      assert.equal(res, fallback)
+      done()
+    })
+  })
+
+})
+
+describe('general', function () {
+  var browser
+
+  beforeEach(function () {
+    browser = Browser()
+  })
+
+  afterEach(function () {
+    browser.close()
+  })
+
+  it('should go to fallback url on an unsupported device', function (done) {
+    browser.go(url, {
+      fallback: fallback,
+      ios_store_link: ios_store_link,
+      android_package_name: android_package_name
+    }, function (res) {
+      assert.equal(res, fallback)
+      done()
+    })
   })
 
 })
